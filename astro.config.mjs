80e7a4ea0ed110917e623753be92951f5d75ba7b
allohamora <herman30698@gmail.com>
2025-02-19 19:45:22 +0200

    ci: configure node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,27 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   env: {
     schema: {
       GROQ_API_KEY: envField.string({ context: 'server', access: 'secret' }),
       GROQ_MODEL: envField.string({ context: 'server', access: 'secret' }),
     },
   },
+
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
   ],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
