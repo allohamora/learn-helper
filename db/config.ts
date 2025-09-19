0e8fc32b746355305e8c340bce5646bd711ed3c6
allohamora <herman30698@gmail.com>
2025-09-19 17:24:48 +0300

    feat: add words

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,24 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable, NOW, sql } from 'astro:db';
+
+// random symbols for id
+const ID = sql`lower(hex(randomblob(16)))`;
+
+const Word = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true, default: ID }),
+    value: column.text(),
+    definition: column.text(),
+    partOfSpeech: column.text({ optional: true }),
+    level: column.text(),
+    spelling: column.text({ optional: true }),
+    pronunciation: column.text(),
+    link: column.text(),
+    list: column.text(),
+    createdAt: column.date({ default: NOW }),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {},
+  tables: { Word },
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,40 @@
-import { db } from 'astro:db';
+import { db, Word } from 'astro:db';
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+
+const getData = async <T>(name: string) => {
+  const path = join(process.cwd(), 'db', 'data', `${name}.json`);
+  const data = await readFile(path, 'utf-8');
+
+  return JSON.parse(data) as T;
+};
+
+type Word = {
+  value: string; // "visit",
+  definition: string; // "an occasion or a period of time when somebody goes to see a place or person and spends time there",
+  partOfSpeech?: string; // "noun",
+  level: string; // "a1",
+  spelling?: string; // "/ˈvɪzɪt/",
+  pronunciation: string; // "https://www.oxfordlearnersdictionaries.com/media/english/us_pron/v/vis/visit/visit__us_1.mp3",
+  link: string; // "https://www.oxfordlearnersdictionaries.com/definition/english/visit_2"
+};
 
 // https://astro.build/db/seed
 export default async function seed() {
-  // TODO
+  const words = await getData<Word[]>('oxford-5000-words');
+  const phrases = await getData<Word[]>('oxford-phrase-list');
+
+  for (const word of words) {
+    await db.insert(Word).values({
+      ...word,
+      list: 'oxford-5000-words',
+    });
+  }
+
+  for (const phrase of phrases) {
+    await db.insert(Word).values({
+      ...phrase,
+      list: 'oxford-phrase-list',
+    });
+  }
 }
