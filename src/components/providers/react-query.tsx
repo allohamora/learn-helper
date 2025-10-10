3634c24654ce9c5f1d0e3da9d5c9d4b6122e4910
allohamora <herman30698@gmail.com>
2025-10-10 20:07:13 +0300

    refactor: rename UserWordsProviderProps to ReactQueryProviderProps

--- src/components/providers/react-query.tsx
+++ src/components/providers/react-query.tsx
@@ -1,11 +1,11 @@
 import { type FC, type ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-type UserWordsProviderProps = {
+type ReactQueryProviderProps = {
   children: ReactNode;
 };
 
-export const ReactQueryProvider: FC<UserWordsProviderProps> = ({ children }) => {
+export const ReactQueryProvider: FC<ReactQueryProviderProps> = ({ children }) => {
   const queryClient = new QueryClient();
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
