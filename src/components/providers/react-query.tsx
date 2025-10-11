afef433ca77282a2542f9c53219508df109c6e10
allohamora <herman30698@gmail.com>
2025-10-11 12:43:41 +0300

    fix: fix queryClient mismatch issue

--- src/components/providers/react-query.tsx
+++ src/components/providers/react-query.tsx
@@ -1,12 +1,12 @@
-import { type FC, type ReactNode } from 'react';
+import { useState, type FC, type ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 type ReactQueryProviderProps = {
   children: ReactNode;
 };
 
-const queryClient = new QueryClient();
-
 export const ReactQueryProvider: FC<ReactQueryProviderProps> = ({ children }) => {
+  const [queryClient] = useState(() => new QueryClient());
+
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
