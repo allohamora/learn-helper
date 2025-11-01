a661b1c3b6e00c7181da86f109f22115ccefb966
allohamora <herman30698@gmail.com>
2025-11-01 12:04:40 +0200

    ci: fix mistakes in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,10 @@
-ssh pi@raspberrypi.local "
-  cd /projects/learn-helper &&
+# bash here to have nvm setup, without it we will get "nvm: command not found"
+ssh pi@raspberrypi.local -t 'bash -i -c "
+  cd ~/projects/learn-helper &&
   git checkout master &&
   git pull &&
   npm ci &&
   npm run push &&
   npm run build &&
-  pm2 restart learn-helper
-"
+  pm2 restart learn-helper --update-env
+"'
