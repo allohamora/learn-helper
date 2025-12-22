791a35abb0a987eb47b06863f5805b319497d151
allohamora <herman30698@gmail.com>
2025-12-22 10:48:24 +0200

    test: split integration and unit tests

--- scripts/jest-debug.sh
+++ scripts/jest-debug.sh
@@ -1,1 +1,16 @@
-eval "npm run test -- -t '$4'"
+# Path to test file supplied by VSCode Jest Runner
+path=$2
+# Test name supplied by VSCode Jest Runner
+testName=$4
+
+type=$(echo "$path" | sed 's/.*\/__tests__\///' | sed 's/\/.*//')
+
+testScriptName() {
+  if [ "$type" = "unit" ]; then
+    echo "test"
+  else
+    echo "test:$type"
+  fi
+}
+
+eval "npm run $(testScriptName) -- $path -t '$testName' --watch --no-cache"
--- vitest.config.ts
+++ vitest.config.ts
@@ -5,21 +5,7 @@ import { getViteConfig } from 'astro/config';
 export default getViteConfig({
   test: {
     watch: false,
-    maxWorkers: 1,
-    minWorkers: 1,
-    poolOptions: {
-      threads: {
-        singleThread: true,
-        minThreads: 1,
-        maxThreads: 1,
-      },
-      forks: {
-        singleFork: true,
-        minForks: 1,
-        maxForks: 1,
-      },
-    },
-    include: ['**/__tests__/integration/**/*.spec.ts', '**/__tests__/unit/**/*.spec.ts'],
+    include: ['**/__tests__/unit/**/*.spec.ts'],
     setupFiles: ['./__tests__/mocks/astro-env.mock.ts'],
   },
   plugins: [tsconfigPathsPlugin()],
