66b212e187622c4c10db669fe2a1f3163ca99e6b
allohamora <herman30698@gmail.com>
2026-06-02 10:54:04 +0300

    chore: migrate to vitest v4

--- vitest-integration.config.ts
+++ vitest-integration.config.ts
@@ -1,4 +1,4 @@
-/// <reference types="vitest" />
+/// <reference types="vitest/config" />
 import tsconfigPathsPlugin from 'vite-tsconfig-paths';
 import { getViteConfig } from 'astro/config';
 
@@ -6,19 +6,7 @@ export default getViteConfig({
   test: {
     watch: false,
     maxWorkers: 1,
-    minWorkers: 1,
-    poolOptions: {
-      threads: {
-        singleThread: true,
-        minThreads: 1,
-        maxThreads: 1,
-      },
-      forks: {
-        singleFork: true,
-        minForks: 1,
-        maxForks: 1,
-      },
-    },
+    isolate: false,
     include: ['**/__tests__/integration/**/*.spec.ts'],
     setupFiles: ['./__tests__/mocks/astro-env.mock.ts'],
   },
