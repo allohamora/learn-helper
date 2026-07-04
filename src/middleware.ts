c3baf89b826c4e8ea41402aed182fe75e23da157
allohamora <herman30698@gmail.com>
2026-07-04 18:01:00 +0300

    chore: remove old implementation

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { clerkMiddleware } from '@clerk/astro/server';
-
-export const onRequest = clerkMiddleware((auth) => {
-  const { isAuthenticated, redirectToSignIn } = auth();
-
-  if (!isAuthenticated) {
-    return redirectToSignIn();
-  }
-});
