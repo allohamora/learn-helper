06fed50da7adeb3316152b5af0d7f49bc488a1e8
allohamora <herman30698@gmail.com>
2026-07-05 17:58:31 +0300

    refactor: remove servers: [{ url: '/api' }],

--- src/server/api.ts
+++ src/server/api.ts
@@ -66,7 +66,6 @@ const app = api.openapi(
 app.doc('/swagger.json', {
   openapi: '3.1.0',
   info: { title: 'Learn Helper API', version: '0.0.1' },
-  servers: [{ url: '/api' }],
 });
 app.get('/swagger', swaggerUI({ url: '/api/swagger.json' }));
 
