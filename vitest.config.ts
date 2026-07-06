b456aacd16b00215f6e336775df9f34971db0afd
allohamora <herman30698@gmail.com>
2026-07-06 09:26:08 +0300

    test: add integration tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -21,12 +21,26 @@ export default defineConfig({
           tsconfigPaths: true,
         },
         test: {
+          name: 'integration',
+          include: ['**/__tests__/integration/**/*.spec.ts'],
+          setupFiles: ['./__tests__/setup-integration-context.ts'],
+          maxWorkers: 5,
+          sequence: {
+            groupOrder: 1,
+          },
+        },
+      },
+      {
+        resolve: {
+          tsconfigPaths: true,
+        },
+        test: {
           name: 'e2e',
           include: ['**/__tests__/e2e/**/*.spec.ts'],
           setupFiles: ['./__tests__/setup-e2e-context.ts'],
           maxWorkers: 5,
           sequence: {
-            groupOrder: 1,
+            groupOrder: 2,
           },
         },
       },
