15a543e942e48f156d83366c908f035a446c4062
allohamora <herman30698@gmail.com>
2026-07-06 09:36:24 +0300

    fix: add correct drizzle schema path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'drizzle-kit';
 import 'dotenv/config';
 
 export default {
-  schema: './src/server/db/schema.ts',
+  schema: './src/server/db/db.schema.ts',
   out: './src/server/db/migrations',
   dialect: 'postgresql',
   casing: 'snake_case',
