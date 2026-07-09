25b2435321784fa6ece4091a1528f2be772d7f3a
allohamora <herman30698@gmail.com>
2026-07-09 17:23:20 +0300

    fix: fix __dirname is not found bug in pino-pretty

--- src/server/utils/logger.utils.ts
+++ src/server/utils/logger.utils.ts
@@ -1,19 +1,10 @@
 import '@tanstack/react-start/server-only';
+import pinoPretty from 'pino-pretty';
 import pino, { type Logger } from 'pino';
 import { PINO_LEVEL } from '../config';
 
 export type { Logger };
 
-const logger = pino({
-  level: PINO_LEVEL,
-  transport: {
-    targets: [
-      {
-        target: 'pino-pretty',
-        level: PINO_LEVEL,
-      },
-    ],
-  },
-});
+const logger = pino({ level: PINO_LEVEL }, pinoPretty());
 
 export const createLogger = (name: string) => logger.child({ name });
