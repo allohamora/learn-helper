96db1bc795f7a76279972333da91f5ca18cfd215
allohamora <herman30698@gmail.com>
2026-07-14 12:04:45 +0300

    docs: note migration-at-startup tradeoff for multi-server deploys

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import handler from '@tanstack/react-start/server-entry';
 import { runMigrations } from '@/server/db/db.service';
 
 // https://tanstack.com/start/latest/docs/framework/react/guide/server-entry-point#server-configuration
+// TODO: if we move to a complex multi-server setup, run this as a separate release step instead
 await runMigrations();
 
 export default handler;
