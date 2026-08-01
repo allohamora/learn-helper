75c22182f826b8a678a1fc6df25895b202538a45
allohamora <herman30698@gmail.com>
2026-08-01 11:50:07 +0000

    chore: add k9s to k3d devcontainer feature

--- .devcontainer/features/k3d/install.sh
+++ .devcontainer/features/k3d/install.sh
@@ -14,7 +14,15 @@ curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 # Install Helm via the official installer
 curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
+# Install k9s, matching the container's architecture
+K9S_VERSION="$(curl -s https://api.github.com/repos/derailed/k9s/releases/latest | grep '"tag_name"' | cut -d '"' -f 4)"
+curl -fsSL -o /tmp/k9s.tar.gz "https://github.com/derailed/k9s/releases/download/${K9S_VERSION}/k9s_Linux_${ARCH}.tar.gz"
+tar -xzf /tmp/k9s.tar.gz -C /tmp k9s
+install -m 0755 /tmp/k9s /usr/local/bin/k9s
+rm -f /tmp/k9s.tar.gz /tmp/k9s
+
 # Verify installations
 kubectl version --client
 k3d version
 helm version
+k9s version
