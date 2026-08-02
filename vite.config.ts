9013c57fa3c15697ede6bf0c31d202b7f56195d0
allohamora <herman30698@gmail.com>
2026-08-02 20:44:14 +0300

    chore: add env loading to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,30 @@
 import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { nitro } from 'nitro/vite';
 import { sentryTanstackStart } from '@sentry/tanstackstart-react/vite';
 
-export default defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [
-    devtools(),
-    tanstackStart(),
-    sentryTanstackStart({
-      org: process.env.SENTRY_ORG,
-      project: process.env.SENTRY_PROJECT,
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-    }),
-    nitro({
-      // https://github.com/nitrojs/nitro/issues/2973
-      preset: './src/server/preset.ts',
-    }),
-    viteReact(),
-    tailwindcss(),
-  ],
+export default defineConfig(({ mode }) => {
+  const { SENTRY_ORG, SENTRY_PROJECT, SENTRY_AUTH_TOKEN } = loadEnv(mode, process.cwd(), '');
+
+  return {
+    resolve: { tsconfigPaths: true },
+    plugins: [
+      devtools(),
+      tanstackStart(),
+      sentryTanstackStart({
+        org: SENTRY_ORG,
+        project: SENTRY_PROJECT,
+        authToken: SENTRY_AUTH_TOKEN,
+      }),
+      nitro({
+        // https://github.com/nitrojs/nitro/issues/2973
+        preset: './src/server/preset.ts',
+      }),
+      viteReact(),
+      tailwindcss(),
+    ],
+  };
 });
